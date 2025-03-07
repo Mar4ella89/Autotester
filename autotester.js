0b1709a2d93ae1f0f75e10c0db9838df0d5c3934
Marat Baikin <3371335@gmail.com>
2025-03-08 01:48:02 +0200

    added testing functions

--- autotester.js
+++ autotester.js
@@ -1,1 +1,49 @@
 console.log("Autotester is started!")
+
+// Проверка наличия rel="preloader"
+function checkPreloader() {
+    const preloader = document.querySelector('link[rel="preloader"]');
+    if (preloader) {
+      console.log('Предзагрузчик найден!');
+    } else {
+      console.log('Предзагрузчик не найден.');
+    }
+  }
+  
+  // Проверка наличия rel="preconnect"
+  function checkPreconnect() {
+    const preconnect = document.querySelector('link[rel="preconnect"]');
+    if (preconnect) {
+      console.log('Preconnect найден!');
+    } else {
+      console.log('Preconnect не найден.');
+    }
+  }
+
+  // Проверка наличия <noscript>
+function checkNoscript() {
+    const noscript = document.querySelector('noscript');
+    if (noscript) {
+      console.log('<noscript> найден!');
+    } else {
+      console.log('<noscript> не найден.');
+    }
+  }
+
+  // Проверка наличия <iframe>
+function checkIframe() {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      console.log('<iframe> найден!');
+    } else {
+      console.log('<iframe> не найден.');
+    }
+  }
+
+  // Вызов всех проверок
+function runTests() {
+    checkPreloader();
+    checkPreconnect();
+    checkNoscript();
+    checkIframe();
+  }
